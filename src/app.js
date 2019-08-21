c2bd1df4b8370b063954817bc91754e1eceb6235
guojingyuan <18637360814@163.com>
2019-08-21 19:00:31 +0800

    环境配置修改

--- src/app.js
+++ src/app.js
@@ -1,46 +1,14 @@
 import React ,{ Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import routeConfig from './routerconfig';
-import Header from '@/component/header'
+import { BrowserRouter as Router } from 'react-router-dom'
+import {renderRoutes} from 'react-router-config';
+import routeConfig from './routerconfig.js';
 
 class App extends Component {
-  _renderRoutes(){
-      let _result = [];
-      let _loop = function(routes,fatherPath){
-          fatherPath = !fatherPath ? "" : fatherPath;
-          routes.map((r) => {
-              if(r.component){
-                  _result.push({
-                      path:fatherPath + r.path,
-                      exact:r.exact,
-                      component:r.component
-                  });
-              }
-              if(r.children){
-                  return _loop(r.children, fatherPath + r.path);
-              }
-          });
-      }
-      _loop(routeConfig);
-      return _result.map((r)=>{
-          return (
-              <Route
-                  key={r.path}
-                  exact={r.exact}
-                  path={r.path}
-                  component={r.component}
-              />
-          )
-      });
-  }
   render(){
       return (
-          <Router basename={process.env.PUBLIC_URL}>
-              <Header></Header>
-              <Switch>
-                  {this._renderRoutes(routeConfig)}
-              </Switch>
-          </Router>
+        <Router>
+            {renderRoutes(routeConfig)}
+        </Router>   
       )
   }
 }
